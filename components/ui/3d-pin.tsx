ad539c9929ef14d59ccb5fff03fcad01e493f8c6
cyrus_w <cyrusw89@gmail.com>
2024-10-10 19:01:08 +1300

    add: apple website project link

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -28,33 +28,34 @@ export const PinContainer = ({
   };
 
   return (
-    <div
-      className={cn(
-        "relative group/pin z-50  cursor-pointer",
-        containerClassName
-      )}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-    >
+    <a href={href} target="_blank" className="block">
       <div
-        style={{
-          perspective: "1000px",
-          transform: "rotateX(70deg) translateZ(0deg)",
-        }}
-        className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
+        className={cn(
+          "relative group/pin z-50 cursor-pointer",
+          containerClassName
+        )}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
       >
         <div
           style={{
-            transform: transform,
+            perspective: "1000px",
+            transform: "rotateX(70deg) translateZ(0deg)",
           }}
-          // remove  bg-black
-          className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
+          className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
         >
-          <div className={cn(" relative z-50 ", className)}>{children}</div>
+          <div
+            style={{
+              transform: transform,
+            }}
+            className="absolute left-1/2 p-4 top-1/2 flex justify-start items-start rounded-2xl shadow-[0_8px_16px_rgb(0_0_0/0.4)] border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
+          >
+            <div className={cn("relative z-50", className)}>{children}</div>
+          </div>
         </div>
+        <PinPerspective title={title} href={href} />
       </div>
-      <PinPerspective title={title} href={href} />
-    </div>
+    </a>
   );
 };
 
@@ -69,7 +70,7 @@ export const PinPerspective = ({
     // change w-96 to w-full
     <motion.div className="pointer-events-none w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
-        <div className="absolute top-0 inset-x-0  flex justify-center">
+        <div className="absolute top-0 inset-x-0 flex justify-center text-nowrap">
           <a
             href={href}
             target={"_blank"}
--- data/index.ts
+++ data/index.ts
@@ -1,8 +1,8 @@
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
   { name: "Experience", link: "#experience" },
+  { name: "Testimonials", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
 ];
 
@@ -73,6 +73,14 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://apple-website-case-study.vercel.app/",
+  },
+  {
+    id: 2,
     title: "3D Solar System Planets to Explore",
     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
     img: "/p1.svg",
@@ -80,7 +88,7 @@ export const projects = [
     link: "",
   },
   {
-    id: 2,
+    id: 3,
     title: "Yoom - Video Conferencing App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
     img: "/p2.svg",
@@ -88,20 +96,12 @@ export const projects = [
     link: "",
   },
   {
-    id: 3,
+    id: 4,
     title: "AI Image SaaS - Canva Application",
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
     img: "/p3.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
     link: "",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    // link: "/ui.apple.com",
   },
 ];
 
